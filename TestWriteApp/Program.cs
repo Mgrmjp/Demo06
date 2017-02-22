3436ea2069fd40417f911f60f9d1df2913f2fd3c
Miikka <K2038@student.jamk.fi>
2017-02-22 21:55:27 +0200

    File writing test.

---
+++ TestWriteApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWriteApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            System.IO.StreamWriter outputFile = new System.IO.StreamWriter(@"E:\Olio-ohjelmointi\Demo06\test.txt");
+            int i = 0;
+
+            do
+            {
+                Console.Write("Give a text line (press enter to end) : ");
+                outputFile.WriteLine(Console.ReadLine());
+                i++;
+            } while (i < 6);
+            outputFile.Close();
+
+            string text = System.IO.File.ReadAllText(@"E:\Olio-ohjelmointi\Demo06\test.txt");
+            Console.WriteLine("Contents of file:\n" + text);
+        }
+    }
+}
